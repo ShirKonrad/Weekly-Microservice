76c800c834fdaf03d6eeab4c00a002e4ea073260
Shir Konrad <shir.konrad@gmail.com>
2023-06-16 00:35:16 +0300

    Add(app): error handling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 
 from flask import Flask, jsonify, request
+from werkzeug.exceptions import HTTPException
 
 from models.Event import Event
 from models.Task import Task
@@ -12,29 +13,56 @@ app = Flask(__name__)
 DATE_FORMAT = '%d.%m.%Y, %H:%M:%S'
 
 
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    print("HTTP ERROR")
+    print(e)
+
+    response = e.get_response()
+
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "message": e.description,
+    })
+    response.content_type = "application/json"
+    return response, e.code
+
+
 @app.route('/assignment', methods=['POST'])
 def get_tasks_assignment():
-    request_data = request.get_json()
-    print(request_data)
+    try:
+        print("------------------------------------------------------")
+        print("Incoming request, time:" + datetime.now().__str__())
+        request_data = request.get_json()
 
-    tasks = []
-    events = []
-    day_hours = (request_data["dayHoursStart"], request_data["dayHoursEnd"])
-    for task in request_data["tasks"]:
-        tasks.append(
-            Task(id=task["id"], duration=task["estTime"], deadline=datetime.strptime(task["dueDate"], DATE_FORMAT),
-                 priority=task["priority"]))
+        tasks = []
+        events = []
 
-    for event in request_data["events"]:
-        events.append(
-            Event(id=event["id"], start_time=datetime.strptime(event["startTime"], DATE_FORMAT),
-                  finish_time=datetime.strptime(event["endTime"], DATE_FORMAT)))
-    print(tasks)
-    print(events)
+        day_hours = (request_data["dayHoursStart"], request_data["dayHoursEnd"])
+        for task in request_data["tasks"]:
+            tasks.append(
+                Task(id=task["id"], duration=task["estTime"], deadline=datetime.strptime(task["dueDate"], DATE_FORMAT),
+                     priority=task["priority"]))
 
-    solution = get_assignment(tasks, events, day_hours)
-    return json.dumps(solution)
+        for event in request_data["events"]:
+            events.append(
+                Event(id=event["id"], start_time=datetime.strptime(event["startTime"], DATE_FORMAT),
+                      finish_time=datetime.strptime(event["endTime"], DATE_FORMAT)))
+        print(tasks)
+        print(events)
 
+        solution = get_assignment(tasks, events, day_hours)
+        print("Returned assignment")
+        return json.dumps(solution)
+
+    except Exception as error:
+        print("An error occurred:", type(error).__name__, "–", error)
+        return json.dumps({
+            "name": type(error).__name__.__str__(),
+            "message": error.__str__(),
+        }), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
